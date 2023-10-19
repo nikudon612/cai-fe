963f6159d64c3d9995d46ffaaae390e2c3fe6128
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2023-10-19 14:37:00 -0400

    added static members images

---
+++ src/components/Members.svelte
@@ -0,0 +1,30 @@
+<script>
+  const members = [
+    {
+      image: "../adobe.jpeg",
+    },
+    {
+      image: "../afp.jpeg",
+    },
+    {
+      image: "../ami.jpeg",
+    },
+    {
+      image: "../anj.jpeg",
+    },
+    {
+      image: "../ap-2x.jpeg",
+    },
+  ];
+</script>
+
+<div>
+  <h2 class="text-xl font-bold">Featured Members</h2>
+  <div class="flex flex-row">
+    {#each members as member}
+      <div class="flex flex-row aspect-square">
+        <img src={member.image} class="aspect-square max-h-[80px] max-w-[80px]" />
+      </div>
+    {/each}
+  </div>
+</div>
--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -1,1 +1,11 @@
-<div>members page</div>
+<script>
+  import Members from "../../components/Members.svelte";
+
+</script>
+<div class="w-full py-6 flex flex-col p-[4vw]">
+    <div class="flex flex-row items-center justify-between w-full mb-[30px]">
+      <a class="font-bold text-3xl" href="/"><h1>Content Authenticity Initiative</h1></a>
+    </div>
+    <h1 class="mb-[60px] text-4xl font-bold">Members</h1>
+    <Members />
+  </div>
