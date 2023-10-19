a5d5d0ff441e575c919548375079cbd7eabe166f
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2023-10-19 15:05:13 -0400

    updates to members section styling

--- src/components/Members.svelte
+++ src/components/Members.svelte
@@ -1,30 +1,45 @@
 <script>
   const members = [
     {
+      name: "adobe",
       image: "../adobe.jpeg",
     },
     {
+    name: "afp",
       image: "../afp.jpeg",
     },
     {
+    name: "ami",
       image: "../ami.jpeg",
     },
     {
+        name: "anj",
       image: "../anj.jpeg",
     },
     {
+        name: "ap",
       image: "../ap-2x.jpeg",
+    },
+    {
+        name: "arm",
+      image: "../arm.jpeg",
     },
   ];
 </script>
 
-<div>
-  <h2 class="text-xl font-bold">Featured Members</h2>
-  <div class="flex flex-row">
-    {#each members as member}
-      <div class="flex flex-row aspect-square">
-        <img src={member.image} class="aspect-square max-h-[80px] max-w-[80px]" />
-      </div>
-    {/each}
+<h2 class="text-xl font-bold text-left">Featured Members</h2>
+<div class="flex flex-col items-center justify-start min-h-screen w-full py-6 p-[4vw]">
+  <div class="w-[80%]">
+    <div class="flex flex-nowrap overflow-x-auto">
+      {#each members as member}
+        <div class="w-[200px] h-[200px] flex p-4">
+          <img
+            src={member.image}
+            alt="Member Image"
+            class="max-w-full max-h-full object-contain"
+          />
+        </div>
+      {/each}
+    </div>
   </div>
 </div>
